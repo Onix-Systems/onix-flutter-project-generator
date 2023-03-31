48132c2df1797c4d6c3de04f43320c7e0f398483
DemeoStep <demeo.alex@gmail.com>
2023-03-31 12:49:54 +0300

    fix: fix flavors case

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/flavors/main_{{#flavors}}{{{.}}}{{/flavors}}.gen.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/flavors/main_{{#flavors}}{{{.}}}{{/flavors}}.gen.dart
@@ -1,11 +1,8 @@
+//@formatter:off
 import 'package:{{project_name}}/flavors.dart';
 import 'package:{{project_name}}/main.dart';
 
 void main() async {
-  F.appFlavor = Flavor
-  .{{#flavors}}{{#upperCase}}{{.}}{{/upperCase}}{{/flavors}};
-  await
-  mainApp
-  (
-  );
+  F.appFlavor = Flavor.{{#flavors}}{{#lowerCase}}{{.}}{{/lowerCase}}{{/flavors}};
+  awaitmainApp();
 }
