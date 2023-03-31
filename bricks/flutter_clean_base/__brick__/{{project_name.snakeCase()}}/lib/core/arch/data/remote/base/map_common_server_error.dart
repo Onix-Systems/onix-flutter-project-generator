ac686da700bf8e9e28e6579bbf30239a88c0f0a3
DemeoStep <demeo.alex@gmail.com>
2023-03-31 13:55:58 +0300

    fix: map_common_server_error.dart

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/arch/data/remote/base/map_common_server_error.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/arch/data/remote/base/map_common_server_error.dart
@@ -1,4 +1,6 @@
-{{#handLocalization}}import 'package:{{project_name}}/core/app/localization/common_app_localization_ext.dart';{{/handLocalization}}
+//@formatter:off
+{{#handLocalization}}import 'package:flutter/widgets.dart';
+import 'package:{{project_name}}/core/app/localization/common_app_localization_ext.dart';{{/handLocalization}}
 {{^handLocalization}}import 'package:{{project_name}}/core/app/localization/generated/l10n.dart';{{/handLocalization}}
 import 'package:{{project_name}}/core/extension/logger_extension.dart';
 import 'package:{{project_name}}/core/arch/data/remote/error/default_api_error.dart';
@@ -9,59 +11,59 @@ import 'package:{{project_name}}/core/di/app.dart';
 
 class MapCommonServerError {
 static String getApiFailureMessage(ApiFailure failure{{#handLocalization}}, BuildContext context{{/handLocalization}}) {
-switch (failure.failure) {
-case ServerFailure.noNetwork:
-{{#handLocalization}}return context.str.apiFailureNoNetwork;{{/handLocalization}}
-{{^handLocalization}}return S.current.apiFailureNoNetwork;{{/handLocalization}}
-case ServerFailure.exception:
-{{#handLocalization}}return context.str.apiFailureUndefined;{{/handLocalization}}
-{{^handLocalization}}return S.current.apiFailureUndefined;{{/handLocalization}}
-case ServerFailure.unAuthorized:
-{{#handLocalization}}return context.str.apiFailureUnAuthorized;{{/handLocalization}}
-{{^handLocalization}}return S.current.apiFailureUnAuthorized;{{/handLocalization}}
-case ServerFailure.tooManyRequests:
-{{#handLocalization}}return context.str.apiFailureTooManyRequests;{{/handLocalization}}
-{{^handLocalization}}return S.current.apiFailureTooManyRequests;{{/handLocalization}}
-case ServerFailure.response:
-return failure.message;
-case ServerFailure.unknown:
-{{#handLocalization}}return context.str.apiFailureUndefined;{{/handLocalization}}
-{{^handLocalization}}return S.current.apiFailureUndefined;{{/handLocalization}}
-}
+  switch (failure.failure) {
+    case ServerFailure.noNetwork:
+      {{#handLocalization}}return context.str.apiFailureNoNetwork;{{/handLocalization}}
+      {{^handLocalization}}return S.current.apiFailureNoNetwork;{{/handLocalization}}
+    case ServerFailure.exception:
+      {{#handLocalization}}return context.str.apiFailureUndefined;{{/handLocalization}}
+      {{^handLocalization}}return S.current.apiFailureUndefined;{{/handLocalization}}
+    case ServerFailure.unAuthorized:
+      {{#handLocalization}}return context.str.apiFailureUnAuthorized;{{/handLocalization}}
+      {{^handLocalization}}return S.current.apiFailureUnAuthorized;{{/handLocalization}}
+    case ServerFailure.tooManyRequests:
+      {{#handLocalization}}return context.str.apiFailureTooManyRequests;{{/handLocalization}}
+      {{^handLocalization}}return S.current.apiFailureTooManyRequests;{{/handLocalization}}
+    case ServerFailure.response:
+      return failure.message;
+    case ServerFailure.unknown:
+      {{#handLocalization}}return context.str.apiFailureUndefined;{{/handLocalization}}
+      {{^handLocalization}}return S.current.apiFailureUndefined;{{/handLocalization}}
+  }
 }
 
 static Failure getServerFailureDetails<T>(DataResponse<T> failure) {
-try {
-return failure.maybeWhen(
-undefinedError: (error) => ApiFailure(
-ServerFailure.unknown,
-message: error.toString(),
-),
-apiError: (error) => _getResponseError(error),
-notConnected: () => ApiFailure(ServerFailure.noNetwork),
-unauthorized: () => ApiFailure(ServerFailure.unAuthorized),
-tooManyRequests: () => ApiFailure(ServerFailure.tooManyRequests),
-orElse: () => ApiFailure(ServerFailure.unknown),
-);
-} catch (e,trace) {
-logger.crash(reason: 'Mapping Error Failed', error: e, stackTrace: trace);
-return ApiFailure(ServerFailure.exception, message: e.toString());
-}
+  try {
+    return failure.maybeWhen(
+      undefinedError: (error) => ApiFailure(
+        ServerFailure.unknown,
+        message: error.toString(),
+      ),
+      apiError: (error) => _getResponseError(error),
+      notConnected: () => ApiFailure(ServerFailure.noNetwork),
+      unauthorized: () => ApiFailure(ServerFailure.unAuthorized),
+      tooManyRequests: () => ApiFailure(ServerFailure.tooManyRequests),
+      orElse: () => ApiFailure(ServerFailure.unknown),
+    );
+  } catch (e,trace) {
+    logger.crash(reason: 'Mapping Error Failed', error: e, stackTrace: trace);
+    return ApiFailure(ServerFailure.exception, message: e.toString());
+  }
 }
 
 static ApiFailure _getResponseError(dynamic customError) {
-if (customError is DefaultApiError) {
-return ApiFailure(
-ServerFailure.response,
-message: customError.name,
-);
-}
-//TODO process other error types and provide results
-//TODO also add new error types to DataResponse if needed
-return ApiFailure(
-ServerFailure.response,
-message: '',
-);
+  if (customError is DefaultApiError) {
+    return ApiFailure(
+      ServerFailure.response,
+      message: customError.name,
+    );
+  }
+  //TODO process other error types and provide results
+  //TODO also add new error types to DataResponse if needed
+  return ApiFailure(
+    ServerFailure.response,
+    message: '',
+  );
 }
 
 }
