4b2ad8344b4fd0ecb3b64a86ced8bd94f7a7a4c1
DemeoStep <demeo.alex@gmail.com>
2023-07-31 17:06:41 +0300

    feat: get app version

--- lib/presentation/screens/splash_screen/splash_screen.dart
+++ lib/presentation/screens/splash_screen/splash_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:onix_flutter_bricks/core/bloc/app_bloc.dart';
 import 'package:onix_flutter_bricks/presentation/screens/main_page/main_screen.dart';
 import 'package:onix_flutter_bricks/presentation/themes/app_colors.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -17,75 +18,90 @@ class _SplashScreenState extends State<SplashScreen> {
   bool logoVisible = false;
   bool buttonVisible = false;
 
+  PackageInfo? packageInfo;
+
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 1)).then((value) {
-      setState(() {
-        logoVisible = true;
+    PackageInfo.fromPlatform().then((value) {
+      Future.delayed(const Duration(seconds: 1)).then((_) {
+        setState(() {
+          packageInfo = value;
+          logoVisible = true;
+        });
       });
     });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedOpacity(
-              opacity: logoVisible ? 0.5 : 0,
-              duration: const Duration(seconds: 2),
-              child: Hero(
-                tag: 'logo',
-                child: SvgPicture.asset(
-                  'assets/logo.svg',
-                  colorFilter: const ColorFilter.mode(
-                    AppColors.white,
-                    BlendMode.srcIn,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedOpacity(
+                  opacity: logoVisible ? 0.5 : 0,
+                  duration: const Duration(seconds: 2),
+                  child: Hero(
+                    tag: 'logo',
+                    child: SvgPicture.asset(
+                      'assets/logo.svg',
+                      colorFilter: const ColorFilter.mode(
+                        AppColors.white,
+                        BlendMode.srcIn,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              onEnd: () {
-                setState(() {
-                  buttonVisible = true;
-                });
-              },
-            ),
-            Visibility(
-              visible: buttonVisible,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: CupertinoButton(
-                  color: AppColors.orange,
-                  onPressed: () {
-                    getDirectoryPath().then(
-                      (value) {
-                        if (value != null) {
-                          Navigator.of(context).pushAndRemoveUntil(
-                            CupertinoPageRoute(
-                              builder: (context) => BlocProvider(
-                                create: (context) =>
-                                    AppBloc(projectPath: value),
-                                child: MainScreen(),
-                              ),
-                            ),
-                            (route) => false,
-                          );
-                        }
-                      },
-                    );
+                  onEnd: () {
+                    setState(() {
+                      buttonVisible = true;
+                    });
                   },
-                  child: const Text(
-                    'Select folder with your projects',
-                    style: TextStyle(color: CupertinoColors.black),
-                  ),
                 ),
-              ),
-            )
-          ],
-        ),
+                Visibility(
+                  visible: buttonVisible,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: CupertinoButton(
+                      color: AppColors.orange,
+                      onPressed: () {
+                        getDirectoryPath().then(
+                          (value) {
+                            if (value != null) {
+                              Navigator.of(context).pushAndRemoveUntil(
+                                CupertinoPageRoute(
+                                  builder: (context) => BlocProvider(
+                                    create: (context) =>
+                                        AppBloc(projectPath: value),
+                                    child: MainScreen(),
+                                  ),
+                                ),
+                                (route) => false,
+                              );
+                            }
+                          },
+                        );
+                      },
+                      child: const Text(
+                        'Select folder with your projects',
+                        style: TextStyle(color: CupertinoColors.black),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            right: 10,
+            bottom: 10,
+            child: Text(packageInfo != null ? 'v${packageInfo!.version}' : ''),
+          ),
+        ],
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import package_info_plus
 import screen_retriever
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
