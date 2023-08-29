b7ca3d57c7c89daea539a5c73ee54677e9a9f31f
DemeoStep <demeo.alex@gmail.com>
2023-08-29 16:40:47 +0300

    feat: working on Makefile

--- bricks/flutter_clean_base/hooks/post_gen.dart
+++ bricks/flutter_clean_base/hooks/post_gen.dart
@@ -465,6 +465,9 @@ flutter_additional_ios_build_settings(target)
         .writeAsString(pbxprojFileContent.replaceAll('''inputFileListPaths = (
 				"\${PODS_ROOT}/Target Support Files/Pods-Runner/Pods-Runner-frameworks-\${CONFIGURATION}-input-files.xcfilelist",
 			);''', '''inputFileListPaths = (
+			);''').replaceAll('''outputFileListPaths = (
+				"\${PODS_ROOT}/Target Support Files/Pods-Runner/Pods-Runner-frameworks-\${CONFIGURATION}-output-files.xcfilelist",
+			);''', '''inputFileListPaths = (
 			);'''));
   }
 }
