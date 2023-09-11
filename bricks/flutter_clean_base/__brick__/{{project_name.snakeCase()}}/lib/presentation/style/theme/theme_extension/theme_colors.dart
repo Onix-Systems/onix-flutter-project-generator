734e2e20b06beecc8c74d3f49205c2785bd487cf
DemeoStep <demeo.alex@gmail.com>
2023-09-11 11:20:42 +0300

    feat: added AppTextField widget to brick

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/presentation/style/theme/theme_extension/theme_colors.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/presentation/style/theme/theme_extension/theme_colors.dart
@@ -18,28 +18,36 @@ class _$ThemeColors {
     AppColors.buttonDisabledLight,
     AppColors.buttonDisabledDark,
   ];
+  static List<Color> borderEnabledColor = [
+    AppColors.textLight,
+    AppColors.textDark,
+  ];
 }{{/theme_generate}}
 {{^theme_generate}}class ThemeColors extends ThemeExtension<ThemeColors> {
   static const light = ThemeColors(
     backgroundColor: AppColors.bgLight,
     buttonColor: AppColors.greenLight,
     buttonDisabledColor: AppColors.buttonDisabledLight,
+    borderEnabledColor: AppColors.textLight,
   );
 
   static const dark = ThemeColors(
     backgroundColor: AppColors.bgDark,
     buttonColor: AppColors.greenDark,
     buttonDisabledColor: AppColors.buttonDisabledDark,
+    borderEnabledColor: AppColors.textDark,
   );
 
   final Color backgroundColor;
   final Color buttonColor;
   final Color buttonDisabledColor;
+  final Color borderEnabledColor;
 
   const ThemeColors({
     required this.backgroundColor,
     required this.buttonColor,
     required this.buttonDisabledColor,
+    required this.borderEnabledColor,
   });
 
   @override
@@ -47,11 +55,13 @@ class _$ThemeColors {
     Color? backgroundColor,
     Color? buttonColor,
     Color? buttonDisabledColor,
+    Color? borderEnabledColor,
   }) {
     return ThemeColors(
       backgroundColor: backgroundColor ?? this.backgroundColor,
       buttonColor: buttonColor ?? this.buttonColor,
       buttonDisabledColor: buttonDisabledColor ?? this.buttonDisabledColor,
+      borderEnabledColor: borderEnabledColor ?? this.borderEnabledColor,
     );
   }
 
@@ -68,6 +78,9 @@ class _$ThemeColors {
       buttonDisabledColor:
       Color.lerp(buttonDisabledColor, other.buttonDisabledColor, t) ??
           buttonDisabledColor,
+      borderEnabledColor:
+      Color.lerp(borderEnabledColor, other.borderEnabledColor, t) ??
+          borderEnabledColor,
     );
   }
 }{{/theme_generate}}
---
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/presentation/widgets/inputs/app_text_field.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:{{project_name}}/presentation/style/theme/theme_extension/ext.dart';
+
+class AppTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final bool enabled;
+  final String label;
+  final BorderRadius? borderRadius;
+  final TextStyle? labelStyle;
+  final Color? fillColor;
+  final VoidCallback? onEditingComplete;
+  final Function(String)? onChanged;
+  final Function(String)? onSubmitted;
+
+  const AppTextField({
+    required this.label,
+    this.enabled = true,
+    this.controller,
+    this.borderRadius,
+    this.labelStyle,
+    this.fillColor,
+    this.onEditingComplete,
+    this.onChanged,
+    this.onSubmitted,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      enabled: enabled,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: borderRadius ?? BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: borderRadius ?? BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: context.appColors.borderEnabledColor,
+          ),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: borderRadius ?? BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: context.appColors.borderEnabledColor.withOpacity(0.2),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: borderRadius ?? BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: context.appColors.buttonColor,
+          ),
+        ),
+        labelText: label,
+        labelStyle: enabled
+            ? labelStyle ?? context.appTextStyles.fs18
+            : labelStyle?.copyWith(
+                    color: labelStyle?.color?.withOpacity(0.2)) ??
+                context.appTextStyles.fs18?.copyWith(
+                  color: context.appTextStyles.fs18?.color?.withOpacity(0.2),
+                ),
+        fillColor: fillColor,
+        filled: fillColor != null,
+      ),
+      onEditingComplete: () => onEditingComplete?.call(),
+      onChanged: (value) => onChanged?.call(value),
+      onSubmitted: (value) => onSubmitted?.call(value),
+    );
+  }
+}
