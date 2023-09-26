de63c9310a09abc1b03427e600bf3ccb0b1e5043
DemeoStep <demeo.alex@gmail.com>
2023-09-26 12:07:41 +0300

    fix: graphQL auth source

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/data/source/remote/auth/auth_source.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/data/source/remote/auth/auth_source.dart
@@ -1,8 +1,8 @@
 import 'package:{{project_name}}/core/arch/domain/entity/common/data_response.dart';
-import 'package:{{project_name}}/data/model/remote/auth/authentication_request.dart';
+import 'package:{{project_name}}/data/model/remote/authentication/authentication_request.dart';
 import 'package:{{project_name}}/data/model/remote/customer_access_token/customer_token_response_data.dart';
 
 abstract class AuthSource {
   Future<DataResponse<CustomerTokenResponseData>> login(
-      AuthRequest requestData);
+      AuthenticationRequest requestData);
 }
