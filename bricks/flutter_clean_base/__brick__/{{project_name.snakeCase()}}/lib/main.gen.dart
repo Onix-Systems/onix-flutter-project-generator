4aa4f094a2178d97cea8cf1f3f9043da0e082bbe
DemeoStep <demeo.alex@gmail.com>
2023-11-01 11:00:44 +0200

    fix: fix imports

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/main.gen.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/main.gen.dart
@@ -1,49 +1,47 @@
+//@formatter:off
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:{{project_name}}/app/banned_app.dart';
 import 'package:{{project_name}}/core/arch/bloc/app_bloc_observer.dart';
 import 'package:{{project_name}}/app/app.dart';
 import 'package:{{project_name}}/app/app_initialization.dart';
+import 'package:{{project_name}}/core/di/services.dart';
 
-Future<void> main
-{{#flavorizr}}
-App{{/flavorizr}}() async
-{
-unawaited
-(
-runZonedGuarded(
-() async {
-WidgetsFlutterBinding.ensureInitialized();
-await Initialization.I.initApp();
-await SystemChrome.setPreferredOrientations([
-DeviceOrientation.portraitUp,
-DeviceOrientation.portraitDown,
-]);
-Bloc.observer = AppBlocObserver();
-final isAllowedToUseApp = await environmentService().initialize();
-if (isAllowedToUseApp) {
-runApp(const App());
-} else {
-runApp(const BannedApp());
-}
-},
-(error, stackTrace) {
-if (kDebugMode) {
-print('runZonedGuarded: Caught error in root zone.\n$error');
-print(stackTrace);
-}
-//there we can add FirebaseCrashlytics recordError method
-},
-)?.catchError((e, trace) {
-if (kDebugMode) {
-print('ERROR: $e');
-print(trace);
-}
-exit(-1);
-})
-,
-);
+Future<void> main{{#flavorizr}}App{{/flavorizr}}() async {
+  unawaited(
+    runZonedGuarded(
+      () async {
+        WidgetsFlutterBinding.ensureInitialized();
+        await Initialization.I.initApp();
+        await SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+        Bloc.observer = AppBlocObserver();
+        final isAllowedToUseApp = await environmentService().initialize();
+        if (isAllowedToUseApp) {
+          runApp(const App());
+        } else {
+          runApp(const BannedApp());
+        }
+        },
+        (error, stackTrace) {
+          if (kDebugMode) {
+            print('runZonedGuarded: Caught error in root zone.\n$error');
+            print(stackTrace);
+          }
+          //there we can add FirebaseCrashlytics recordError method
+        },
+        )?.catchError((e, trace) {
+          if (kDebugMode) {
+            print('ERROR: $e');
+            print(trace);
+          }
+      exit(-1);
+    }),
+  );
 }
