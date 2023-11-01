ca2c3ce9034369e3fcd2a810a1df6ca18703cf5d
DemeoStep <demeo.alex@gmail.com>
2023-11-01 09:30:54 +0200

    fix: getApiFailureMessage moved to app level extension

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/arch/data/remote/base/map_common_server_error.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/arch/data/remote/base/map_common_server_error.dart
@@ -10,27 +10,6 @@ import 'package:{{project_name}}/core/arch/domain/entity/failure/failure.dart';
 import 'package:{{project_name}}/core/di/app.dart';
 
 class MapCommonServerError {
-static String getApiFailureMessage(ApiFailure failure{{#handLocalization}}, BuildContext context{{/handLocalization}}) {
-  switch (failure.failure) {
-    case ServerFailure.noNetwork:
-      {{#handLocalization}}return context.str.apiFailureNoNetwork;{{/handLocalization}}
-      {{^handLocalization}}return S.current.apiFailureNoNetwork;{{/handLocalization}}
-    case ServerFailure.exception:
-      {{#handLocalization}}return context.str.apiFailureUndefined;{{/handLocalization}}
-      {{^handLocalization}}return S.current.apiFailureUndefined;{{/handLocalization}}
-    case ServerFailure.unAuthorized:
-      {{#handLocalization}}return context.str.apiFailureUnAuthorized;{{/handLocalization}}
-      {{^handLocalization}}return S.current.apiFailureUnAuthorized;{{/handLocalization}}
-    case ServerFailure.tooManyRequests:
-      {{#handLocalization}}return context.str.apiFailureTooManyRequests;{{/handLocalization}}
-      {{^handLocalization}}return S.current.apiFailureTooManyRequests;{{/handLocalization}}
-    case ServerFailure.response:
-      return failure.message;
-    case ServerFailure.unknown:
-      {{#handLocalization}}return context.str.apiFailureUndefined;{{/handLocalization}}
-      {{^handLocalization}}return S.current.apiFailureUndefined;{{/handLocalization}}
-  }
-}
 
 static Failure getServerFailureDetails<T>(DataResponse<T> failure) {
   try {
