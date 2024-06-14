f530b0f2ceab2f000dbc3636b3c1d2be198f42fd
alexandr.fedorenko <fedorenkoalex1992@gmail.com>
2024-06-14 11:17:21 +0300

    feat(update_27): fix issue/76

--- lib/presentation/screen/screens_screen/widgets/screen_table.dart
+++ lib/presentation/screen/screens_screen/widgets/screen_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:onix_flutter_bricks/core/app/localization/generated/l10n.dart';
 import 'package:onix_flutter_bricks/domain/entity/screen/screen.dart';
@@ -82,128 +83,130 @@ class ScreenTable extends StatelessWidget {
             ],
           ),
         ),
-        ListView(
-          padding: EdgeInsets.zero,
-          shrinkWrap: true,
-          children: [
-            ...screens.map(
-              (screen) => Container(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                ),
-                decoration:  BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: context.appColors.fadedColor,
-                      width: 1,
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            children: [
+              ...screens.map(
+                (screen) => Container(
+                  padding: const EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                  ),
+                  decoration:  BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: context.appColors.fadedColor,
+                        width: 1,
+                      ),
                     ),
                   ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Cell(
-                      value: MSHCheckbox(
-                        value: screen.initial,
-                        duration: const Duration(milliseconds: 200),
-                        colorConfig:
-                            MSHColorConfig.fromCheckedUncheckedDisabled(
-                          checkedColor: context.appColors.controlColor,
-                          uncheckedColor: context.appColors.controlColor,
-                          disabledColor: context.appColors.controlColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Cell(
+                        value: MSHCheckbox(
+                          value: screen.initial,
+                          duration: const Duration(milliseconds: 200),
+                          colorConfig:
+                              MSHColorConfig.fromCheckedUncheckedDisabled(
+                            checkedColor: context.appColors.controlColor,
+                            uncheckedColor: context.appColors.controlColor,
+                            disabledColor: context.appColors.controlColor,
+                          ),
+                          onChanged: (value) {
+                            onChangeInitial(screen);
+                          },
+                          size: 20,
                         ),
-                        onChanged: (value) {
-                          onChangeInitial(screen);
-                        },
-                        size: 20,
+                        decorated: true,
                       ),
-                      decorated: true,
-                    ),
-                    Cell(
-                      value: Text(
-                        '${screen.name.pascalCase}Screen',
-                        style: context.appTextStyles.fs18?.copyWith(
-                            color: screen.exists
-                                ? context.appColors.fadedColor
-                                : context.appColors.textColor),
+                      Cell(
+                        value: Text(
+                          '${screen.name.pascalCase}Screen',
+                          style: context.appTextStyles.fs18?.copyWith(
+                              color: screen.exists
+                                  ? context.appColors.fadedColor
+                                  : context.appColors.textColor),
+                        ),
+                        decorated: true,
                       ),
-                      decorated: true,
-                    ),
-                    Cell(
-                      value: Text(
-                        screen.stateManager.name.pascalCase,
-                        style: context.appTextStyles.fs18?.copyWith(
-                            color: screen.exists
-                                ? context.appColors.fadedColor
-                                : context.appColors.textColor),
+                      Cell(
+                        value: Text(
+                          screen.stateManager.name.pascalCase,
+                          style: context.appTextStyles.fs18?.copyWith(
+                              color: screen.exists
+                                  ? context.appColors.fadedColor
+                                  : context.appColors.textColor),
+                        ),
+                        decorated: true,
                       ),
-                      decorated: true,
-                    ),
-                    Cell(
-                      value: SizedBox(
-                        height: 45,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              CupertinoButton(
-                                color: screen.exists
-                                    ? context.appColors.fadedColor
-                                    : context.appColors.textColor,
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                onPressed: () {
-                                  if (!screen.exists) {
-                                    showCupertinoModalPopup<Screen>(
-                                      context: context,
-                                      barrierDismissible: false,
-                                      builder: (context) => AddScreenDialog(
-                                          screen: Screen.copyOf(screen)),
-                                    ).then((modifiedScreen) {
-                                      if (modifiedScreen != null) {
-                                        onModifyScreen(
-                                            modifiedScreen, screen.name);
-                                      }
-                                    });
-                                  }
-                                },
-                                child: Text(
-                                  S.of(context).modify,
-                                  style: context.appTextStyles.fs18?.copyWith(
-                                      color: context.appColors.darkColor),
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              CupertinoButton(
-                                color: screen.exists
-                                    ? context.appColors.fadedColor
-                                    : context.appColors.textColor,
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                onPressed: () {
-                                  if (!screen.exists) {
-                                    onDeleteScreen(screen);
-                                  }
-                                },
-                                child: Text(
-                                  S.of(context).delete,
-                                  style: context.appTextStyles.fs18?.copyWith(
-                                    color: context.appColors.darkColor,
+                      Cell(
+                        value: SizedBox(
+                          height: 45,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CupertinoButton(
+                                  color: screen.exists
+                                      ? context.appColors.fadedColor
+                                      : context.appColors.textColor,
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 10),
+                                  onPressed: () {
+                                    if (!screen.exists) {
+                                      showCupertinoModalPopup<Screen>(
+                                        context: context,
+                                        barrierDismissible: false,
+                                        builder: (context) => AddScreenDialog(
+                                            screen: Screen.copyOf(screen)),
+                                      ).then((modifiedScreen) {
+                                        if (modifiedScreen != null) {
+                                          onModifyScreen(
+                                              modifiedScreen, screen.name);
+                                        }
+                                      });
+                                    }
+                                  },
+                                  child: Text(
+                                    S.of(context).modify,
+                                    style: context.appTextStyles.fs18?.copyWith(
+                                        color: context.appColors.darkColor),
                                   ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(width: 10),
+                                CupertinoButton(
+                                  color: screen.exists
+                                      ? context.appColors.fadedColor
+                                      : context.appColors.textColor,
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 10),
+                                  onPressed: () {
+                                    if (!screen.exists) {
+                                      onDeleteScreen(screen);
+                                    }
+                                  },
+                                  child: Text(
+                                    S.of(context).delete,
+                                    style: context.appTextStyles.fs18?.copyWith(
+                                      color: context.appColors.darkColor,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ],
     );
