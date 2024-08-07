2a303a9f504d825e730eb4ff21d220978b82defc
Ivan Modlo <vanek975@gmail.com>
2024-08-07 20:14:29 +0300

    feat(Base Arch): Updated code

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/arch/bloc/base_cubit_state.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/arch/bloc/base_cubit_state.dart
@@ -3,6 +3,7 @@ import 'package:{{project_name}}/core/arch/bloc/base_cubit.dart';
 import 'package:{{project_name}}/core/arch/bloc/bloc_builders_mixin.dart';
 import 'package:{{project_name}}/core/arch/bloc/bloc_typedefs.dart';
 import 'package:{{project_name}}/core/arch/bloc/stream_listener.dart';
+import 'package:{{project_name}}/core/arch/domain/entity/progress_state/progress_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -41,8 +42,8 @@ W extends StatefulWidget> extends State<W>
     {{^web_only}}if (context.mounted) {
     context.loaderOverlay.hide();
     }{{/web_only}}
-    if (_bloc != null) {
-    _bloc?.dispose();
+    if (_cubit != null) {
+    _cubit?.dispose();
     }
     super.dispose();
   }
@@ -65,90 +66,17 @@ W extends StatefulWidget> extends State<W>
     );
   }
 
-
-  void onCubitCreated(BuildContext context, C bloc) {
-    bloc.progressStream.listen((event) async {
-      if (event.showProgress) {
-        {{#web_only}}context.progressShow();{{/web_only}}
+  void onBlocCreated(BuildContext context, C cubit) {
+    cubit.progressStream.listen((event) async {
+      if (event is DefaultProgressState) {
+        if (event.showProgress) {
+          {{#web_only}}context.progressShow();{{/web_only}}
       {{^web_only}}context.loaderOverlay.show();{{/web_only}}
       } else {
       {{#web_only}}context.progressHide();{{/web_only}}
       {{^web_only}}context.loaderOverlay.hide();{{/web_only}}
       }
-    });
-  }
-
-  // ignore: no-empty-block
-  void initParams(BuildContext context) {}
-
-  Widget buildWidget(BuildContext context);
-}
-
-
-abstract class BaseCubitState<S, C extends BaseCubit<S, SR>, SR,
-W extends StatefulWidget> extends State<W>
-    with BlocBuildersMixin<C, S, SR> {
-  bool lazyCubit = false;
-  C? _cubit;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider<C>(
-      create: (context) {
-        final cubit = createCubit();
-        onCubitCreated(context, cubit);
-        _cubit = cubit;
-        return cubit;
-      },
-      lazy: lazyCubit,
-      child: Builder(
-        builder: (context) {
-          initParams(context);
-          return buildWidget(context);
-        },
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    {{#web_only}}Loader.hide();{{/web_only}}
-    {{^web_only}}if (context.mounted) {
-    context.loaderOverlay.hide();
-    }{{/web_only}}
-    if (_bloc != null) {
-    _bloc?.dispose();
     }
-    super.dispose();
-  }
-
-  B cubitOf(BuildContext context) => context.read<B>();
-
-  B createCubit() => GetIt.I.get<B>();
-
-  Widget srObserver({
-    required BuildContext context,
-    required Widget child,
-    required SingleResultListener<SR> onSR,
-  }) {
-    return StreamListener<SR>(
-      stream: (_cubit ?? cubitOf(context)).singleResults,
-      onData: (data) {
-        onSR(context, data);
-      },
-      child: child,
-    );
-  }
-
-  void onCubitCreated(BuildContext context, B bloc) {
-    bloc.progressStream.listen((event) async {
-      if (event) {
-        {{#web_only}}context.progressShow();{{/web_only}}
-      {{^web_only}}context.loaderOverlay.show();{{/web_only}}
-      } else {
-      {{#web_only}}context.progressHide();{{/web_only}}
-      {{^web_only}}context.loaderOverlay.hide();{{/web_only}}
-      }
     });
   }
 
