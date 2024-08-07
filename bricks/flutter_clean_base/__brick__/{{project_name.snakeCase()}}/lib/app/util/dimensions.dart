b99ef5722a3ed8a81aa7602e1ff293b56244e57d
Ivan Modlo <vanek975@gmail.com>
2024-08-07 14:46:58 +0300

    fix(Dimensions): Added correct constructor for the Dimension class

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/app/util/dimensions.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/app/util/dimensions.dart
@@ -1,8 +1,8 @@
 {{^web_only}}import 'package:flutter_screenutil/flutter_screenutil.dart';{{/web_only}}
 
 final class Dimensions {
-{{^web_only}}static final delimiterH10 = 10.h;{{/web_only}}
-{{#web_only}}static final delimiterH10 = 10.0;{{/web_only}}
+{{^web_only}} static final delimiterH10 = 10.h;{{/web_only}}
+{{#web_only}} static final delimiterH10 = 10.0;{{/web_only}}
 
-  const QaUtils._();
+  const Dimensions._();
 }
