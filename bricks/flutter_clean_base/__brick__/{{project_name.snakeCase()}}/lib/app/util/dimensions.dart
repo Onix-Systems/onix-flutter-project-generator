11eadfc6b7e4e0b091156b65a6c385a851a596bc
Ivan Modlo <vanek975@gmail.com>
2024-08-08 14:09:43 +0300

    feat(Screen Unit): added const modifier and updated statement

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/app/util/dimensions.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/app/util/dimensions.dart
@@ -1,8 +1,8 @@
-{{^screen_util}}import 'package:flutter_screenutil/flutter_screenutil.dart';{{/screen_util}}
+{{#screen_util}}import 'package:flutter_screenutil/flutter_screenutil.dart';{{/screen_util}}
 
 final class Dimensions {
 {{#screen_util}} static final delimiterH10 = 10.h;{{/screen_util}}
-{{^screen_util}} static final delimiterH10 = 10.0;{{/screen_util}}
+{{^screen_util}} static const delimiterH10 = 10.0;{{/screen_util}}
 
   const Dimensions._();
 }
