e77fbbb1039463a2d173496dbfa9d2e502842583
Saif Ali Aljanahi <saif.ali@alqaseh.com>
2024-08-24 00:20:52 +0300

    fix: use runZoneGuarded to run first

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/main.gen.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/main.gen.dart
@@ -1,47 +1,46 @@
 //@formatter:off
+import 'package:flutter/widgets.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'dart:async';
 import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:{{project_name}}/app/banned_app.dart';
-import 'package:{{project_name}}/core/arch/bloc/app_bloc_observer.dart';
-import 'package:{{project_name}}/app/app.dart';
-import 'package:{{project_name}}/app/app_initialization.dart';
-import 'package:{{project_name}}/core/di/services.dart';
-import 'package:{{project_name}}/app/util/extension/orientation_extension.dart';
-import 'package:{{project_name}}/core/arch/logger/app_logger_impl.dart';
 
-Future<void> main{{#flavorizr}}App{{/flavorizr}}() async {
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = const String.fromEnvironment(AppService.sentryDsn);
-    },
-    appRunner: () {
-      unawaited(
-        runZonedGuarded(
-          () async {
-            WidgetsFlutterBinding.ensureInitialized();
-            await Initialization.I.initApp();
+import 'app_service.dart'; // Assuming this is where AppService is defined
+import 'logger.dart'; // Assuming this is where logger is defined
+import 'initialization.dart'; // Assuming this is where Initialization is defined
+import 'orientation_extension.dart'; // Assuming this is where OrientationExtension is defined
+import 'app_bloc_observer.dart'; // Assuming this is where AppBlocObserver is defined
+import 'environment_service.dart'; // Assuming this is where environmentService is defined
+import 'app.dart'; // Assuming this is where the App and BannedApp widgets are defined
 
-            await OrientationExtension.lockVertical();
+Future<void> main() async {
+  //Sentry docs suggested to wrap the main function with runZonedGuarded
+  // see https://github.com/getsentry/sentry-dart/blob/main/flutter/README.md
+  runZonedGuarded(() async {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = const String.fromEnvironment(AppService.sentryDsn);
+      },
+    );
+    WidgetsFlutterBinding.ensureInitialized();
+    await Initialization.I.initApp();
+    await OrientationExtension.lockVertical();
 
-            Bloc.observer = AppBlocObserver();
-            final isAllowedToUseApp = await environmentService().initialize();
-            if (isAllowedToUseApp) {
-              runApp(const App());
-            } else {
-              runApp(const BannedApp());
-            }
-          },
-          (error, stackTrace) {
-            logger.crash(error: error, stackTrace: stackTrace, reason: 'main');
-          },
-        )?.catchError((e, trace) {
-          logger.crash(error: e, stackTrace: trace, reason: 'main');
-          exit(-1);
-        }),
+    Bloc.observer = AppBlocObserver();
+    final isAllowedToUseApp = await environmentService().initialize();
+    if (isAllowedToUseApp) {
+      runApp(
+        DefaultAssetBundle(
+          bundle: SentryAssetBundle(),
+          child: MyApp(),
+        ),
       );
-    },
-  );
+    } else {
+      runApp(const BannedApp());
+    }
+  }, (exception, stackTrace) async {
+    await Sentry.captureException(exception, stackTrace: stackTrace);
+    logger.crash(error: exception, stackTrace: stackTrace, reason: 'main');
+    exit(-1);
+  });
 }
+
