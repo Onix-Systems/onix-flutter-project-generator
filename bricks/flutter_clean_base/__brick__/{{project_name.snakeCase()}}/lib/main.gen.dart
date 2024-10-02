bbe27a428e9eb4b88b66d36ed1cdb0a5c72df409
Ivan <vanek975@gmail.com>
2024-10-02 16:56:31 +0300

    feat(Sentry): changed logic

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/main.gen.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/main.gen.dart
@@ -10,7 +10,7 @@ import 'package:{{project_name}}/app/app_initialization.dart';
 import 'package:{{project_name}}/core/di/services.dart';
 import 'package:{{project_name}}/app/util/extension/orientation_extension.dart';
 import 'package:{{project_name}}/core/arch/logger/app_logger_impl.dart';
-{{^sentry}}import 'package:sentry_flutter/sentry_flutter.dart';{{/sentry}}
+{{#sentry}}import 'package:sentry_flutter/sentry_flutter.dart';{{/sentry}}
 
 Future<void> main{{#flavorizr}}App{{/flavorizr}}() async {
   unawaited(
@@ -34,8 +34,9 @@ Future<void> main{{#flavorizr}}App{{/flavorizr}}() async {
           runApp(const BannedApp());
         }
         },
-        (error, stackTrace) => _onError(error, stackTrace),
-
+        (error, stackTrace) {
+          _onError(error, stackTrace);
+        },
         )?.catchError((error, stackTrace) {
           _onError(error, stackTrace);
           exit(-1);
@@ -45,6 +46,6 @@ Future<void> main{{#flavorizr}}App{{/flavorizr}}() async {
 
   Future<void> _onError(dynamic error, dynamic stackTrace) async {
     logger.crash(error: error, stackTrace: stackTrace, reason: 'main');
-    {{^sentry}}await Sentry.captureException(exception, stackTrace: stackTrace);{{/sentry}}
+    {{#sentry}}await Sentry.captureException(exception, stackTrace: stackTrace);{{/sentry}}
   }
 }
