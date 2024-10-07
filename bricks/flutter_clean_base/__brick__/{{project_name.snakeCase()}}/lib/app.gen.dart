8ca80271b39629437baa4b939ef52132e9b8248e
DemeoStep <demeo.alex@gmail.com>
2024-10-07 11:14:34 +0300

    feat: refactoring brick

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/app.gen.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/app.gen.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 {{^web_only}}import 'package:loader_overlay/loader_overlay.dart';{{/web_only}}
 {{#screen_util}}import 'package:flutter_screenutil/flutter_screenutil.dart';{{/screen_util}}
 {{#isBloc}}import 'package:{{project_name}}/core/arch/bloc/base_bloc_state.dart';
-import 'package:{{project_name}}/core/arch/widget/common/theme_switcher.dart';{{/isBloc}}
+import 'package:{{project_name}}/app/bloc/app_bloc_imports.dart';{{/isBloc}}
 {{#isProvider}}import 'package:{{project_name}}/core/arch/provider/base_provider_state.dart';
 import 'package:{{project_name}}/app/provider/app_provider_imports.dart';{{/isProvider}}
 import 'package:{{project_name}}/presentation/style/theme/theme_imports.dart';
-{{#isBase}}import 'package:{{project_name}}/app/core/widget/common/theme_switcher.dart';{{/isBase}}
+{{#isBase}}import 'package:{{project_name}}/core/arch/widget/common/theme_switcher.dart';{{/isBase}}
 {{#isGoRouter}}import 'package:{{project_name}}/app/router/app_router.dart';{{/isGoRouter}}
 {{^isGoRouter}}import 'package:{{project_name}}/core/di/app.dart';{{/isGoRouter}}
 {{^handLocalization}}import 'package:flutter_localizations/flutter_localizations.dart';{{/handLocalization}}
