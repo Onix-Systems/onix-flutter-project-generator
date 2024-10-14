a676a77deb18fe77abbb45912f883b8fd4de8379
DemeoStep <demeo.alex@gmail.com>
2024-10-14 14:34:01 +0300

    feat: remove app_provider, refactoring post_gen.dart

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/di/riverpod.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/di/riverpod.dart
@@ -5,13 +5,5 @@ import 'package:{{project_name}}/app/riverpod/app_provider_imports.dart';
 //{imports end}
 
 void registerRiverpod(GetIt getIt) {
-  getIt.registerSingleton<StateNotifierProvider<AppProvider, AppState>>(
-    StateNotifierProvider<AppProvider, AppState>(
-      (ref) => AppProvider(),
-    ),
-  );
   //{riverpod end}
 }
-
-StateNotifierProvider<AppProvider, AppState> appProvider() =>
-    GetIt.I.get<StateNotifierProvider<AppProvider, AppState>>();
