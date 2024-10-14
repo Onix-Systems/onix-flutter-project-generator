462b53bc2102706010c2eb0cd720cad0681fa00c
DemeoStep <demeo.alex@gmail.com>
2024-10-14 14:41:24 +0300

    feat: remove app_provider, refactoring post_gen.dart

--- bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/di/riverpod.dart
+++ bricks/flutter_clean_base/__brick__/{{project_name.snakeCase()}}/lib/core/di/riverpod.dart
@@ -1,7 +1,6 @@
 //@formatter:off
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
-import 'package:{{project_name}}/app/riverpod/app_provider_imports.dart';
 //{imports end}
 
 void registerRiverpod(GetIt getIt) {
