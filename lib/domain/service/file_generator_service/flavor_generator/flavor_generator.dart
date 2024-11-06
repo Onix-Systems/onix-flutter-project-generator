75722dd9382cc6ab9cd35a0a06d46f20623e7753
DemeoStep <demeo.alex@gmail.com>
2024-11-06 14:53:05 +0200

    feat: refactoring

--- lib/domain/service/file_generator_service/flavor_generator/flavor_generator.dart
+++ lib/domain/service/file_generator_service/flavor_generator/flavor_generator.dart
@@ -6,16 +6,18 @@ import 'package:onix_flutter_bricks/app/extension/process_extension.dart';
 import 'package:onix_flutter_bricks/core/di/app.dart';
 import 'package:onix_flutter_bricks/domain/entity/arch_type/arch_type.dart';
 import 'package:onix_flutter_bricks/domain/entity/failure/flavorizing_failure.dart';
+import 'package:onix_flutter_bricks/domain/entity/platforms_list/platforms_list.dart';
 import 'package:onix_flutter_bricks/domain/service/base/base_generation_service.dart';
+import 'package:onix_flutter_bricks/domain/service/file_generator_service/flavor_generator/mixins/flavor_generator_utils_mixin.dart';
+import 'package:onix_flutter_bricks/domain/service/file_generator_service/flavor_generator/mixins/flavor_injector_mixin.dart';
 import 'package:onix_flutter_bricks/domain/service/file_generator_service/flavor_generator/params/flavor_generator_params.dart';
 import 'package:onix_flutter_bricks/domain/service/output_service/output_service.dart';
-import 'package:onix_flutter_bricks/util/extension/swagger_extensions.dart';
 import 'package:onix_flutter_core/onix_flutter_core.dart';
-import 'package:recase/recase.dart';
 
 ///This class generates flavors for the project
 class FlavorGenerator
-    implements BaseGenerationService<Result<int>, FlavorGeneratorParams> {
+    extends BaseGenerationService<Result<int>, FlavorGeneratorParams>
+    with FlavorInjectorMixin, FlavorGeneratorUtilsMixin {
   final OutputService _outputService;
   static const flavorizrInjectKey = '#{flavorizer_injection_config}';
 
@@ -57,7 +59,7 @@ class FlavorGenerator
 
       _outputService.add('{#info}Arch type: ${archType?.name ?? 'unknown'}');
 
-      final org = await _getOrg(params.projectFolder);
+      final org = await getOrg(params.projectFolder);
 
       if (org.isEmpty) {
         return Result.error(
@@ -67,20 +69,23 @@ class FlavorGenerator
 
       _outputService.add('{#info}Org: $org');
 
-      // return const Result.success(0);
-
       final isIOsEnabled = Directory('$projectPath/ios').existsSync();
 
       final isAndroidEnabled = Directory('$projectPath/android').existsSync();
 
       final isMacOsEnabled = Directory('$projectPath/macos').existsSync();
 
-      await _injectFlavors(
-        params,
-        org,
-        isIOsEnabled,
-        isAndroidEnabled,
-        isMacOsEnabled,
+      final platforms = PlatformsList(
+        android: isAndroidEnabled,
+        ios: isIOsEnabled,
+        macos: isMacOsEnabled,
+      );
+
+      await injectFlavors(
+        params: params,
+        org: org,
+        platformsList: platforms,
+        isGenerated: isGenerated,
       );
 
       ///Manipulations with icons
@@ -88,7 +93,7 @@ class FlavorGenerator
         await Directory('$projectPath/flavor_assets').create(recursive: true);
 
         for (final flavor in params.flavors) {
-          await _copyIcons(projectPath, flavor, params);
+          await copyIcons(projectPath, flavor, params);
         }
       }
 
@@ -193,51 +198,26 @@ class FlavorGenerator
             '$projectPath/lib/${isGenerated ? 'app/' : ''}flavors/main_$flavor.dart',
           ).writeAsString(mainFlavorFileContent);
 
-          if (isIOsEnabled) {
-            await _correctApple(
-              projectPath: projectPath,
-              flavor: flavor,
-              isGenerated: isGenerated,
-              platform: 'ios',
-            );
-          }
+          for (final platform in platforms.asList()) {
+            if (platform == 'android') continue;
 
-          if (isMacOsEnabled) {
-            await _correctApple(
+            await correctApple(
               projectPath: projectPath,
               flavor: flavor,
               isGenerated: isGenerated,
-              platform: 'macos',
+              platform: platform,
             );
           }
         }
 
-        await Process.run(
-          'rm',
-          ['-r', 'pages'],
-          workingDirectory: '$projectPath/lib',
+        await clean(
+          projectPath: projectPath,
+          isGenerated: isGenerated,
         );
-        await Process.run(
-          'rm',
-          ['main_dart.xml'],
-          workingDirectory: '$projectPath/.idea/runConfigurations',
-        );
-        if (isGenerated) {
-          await Process.run(
-            'rm',
-            ['ic_launcher.png'],
-            workingDirectory: '$projectPath/assets/launcher_icons',
-          );
-          await Process.run(
-            'rm',
-            ['app.dart'],
-            workingDirectory: '$projectPath/lib',
-          );
-        }
 
         _outputService.add('{#info}Flavorizing finished');
 
-        await _createFlavorBannerWidget(
+        await createFlavorBannerWidget(
           projectPath: projectPath,
           projectName: projectName,
           isGenerated: isGenerated,
@@ -257,250 +237,6 @@ class FlavorGenerator
         failure: FlavorizingFailure(FlavorizingFailureType.exception),
       );
     }
-  }
-
-  Future<String> _getOrg(String name) async {
-    final appBuildGradleFile = File('$name/android/app/build.gradle');
-    var pbxProjFile = File('$name/ios/Runner.xcodeproj/project.pbxproj');
-
-    if (!pbxProjFile.existsSync()) {
-      pbxProjFile = File('$name/macos/Runner.xcodeproj/project.pbxproj');
-    }
-
-    if (appBuildGradleFile.existsSync()) {
-      final appBuildGradleContent = await appBuildGradleFile.readAsString();
-      final appBuildGradleContentLines = appBuildGradleContent.split('\n');
-
-      final applicationIdLine = appBuildGradleContentLines
-          .firstWhereOrNull((element) => element.contains('applicationId = '));
-
-      if (applicationIdLine != null) {
-        final lines = applicationIdLine
-            .split('applicationId = ')
-            .last
-            .replaceAll('"', '')
-            .split('.');
-
-        return lines.sublist(0, lines.length - 1).join('.');
-      }
-    }
-
-    if (pbxProjFile.existsSync()) {
-      final pbxProjContent = await pbxProjFile.readAsString();
-      final pbxProjContentLines = pbxProjContent.split('\n');
-
-      final applicationIdLines = pbxProjContentLines
-          .where((element) => element.contains('PRODUCT_BUNDLE_IDENTIFIER = '))
-          .map(
-            (e) => e
-                .replaceFirst('.RunnerTests', '')
-                .replaceLast(';', '')
-                .split('PRODUCT_BUNDLE_IDENTIFIER = ')
-                .last
-                .split('.'),
-          )
-          .toSet();
-
-      if (applicationIdLines.isEmpty) return '';
-
-      var orgSegments = applicationIdLines.first.toSet();
-
-      for (final org in applicationIdLines) {
-        orgSegments = orgSegments.intersection(org.toSet());
-      }
-
-      return orgSegments.toList().getRange(0, orgSegments.length - 1).join('.');
-    }
-
-    return '';
-  }
-
-  Future<void> _injectFlavors(
-    FlavorGeneratorParams params,
-    String org,
-    bool isIOsEnabled,
-    bool isAndroidEnabled,
-    bool isMacOsEnabled,
-  ) async {
-    ///START:Flavorizer config injection
-    final isFlavorized = params.flavors.isNotEmpty;
-
-    final isGenerated = _isGenerated(params.projectFolder);
-
-    final name = params.projectFolder;
-
-    final projectName = name.split('/').last;
-
-    final pubspecFile = File('$name/pubspec.yaml');
-    if (!pubspecFile.existsSync()) return;
-    final pubspecFileContent = await pubspecFile.readAsString();
-    if (isFlavorized) {
-      final flavors = params.flavors.toList();
-
-      final lines = List<String>.empty(growable: true)
-        ..add('flavorizr:')
-        ..add('  app:')
-        ..add('    android:')
-        ..add('      flavorDimensions: "flavor-type"')
-        ..add('')
-        ..add('  flavors:');
-      for (final flavor in flavors) {
-        final packageSuffix = flavor.toLowerCase() == 'prod' ? '' : '.$flavor';
-        final nameSuffix =
-            flavor.toLowerCase() == 'prod' ? '' : ' ${flavor.titleCase}';
-        lines
-          ..add('    $flavor:')
-          ..add('      app:')
-          ..add('        name: "${projectName.titleCase}$nameSuffix"')
-          ..add('');
-        if (isAndroidEnabled) {
-          lines
-            ..add('      android:')
-            ..add('        applicationId: "$org.$projectName$packageSuffix"')
-            ..add(
-              isGenerated
-                  ? '        icon: "flavor_assets/$flavor/launcher_icons/ic_launcher.png"'
-                  : '',
-            )
-            ..add('');
-        }
-        if (isIOsEnabled) {
-          lines
-            ..add('      ios:')
-            ..add('        bundleId: "$org.$projectName$packageSuffix"')
-            ..add(
-              isGenerated
-                  ? '        icon: "flavor_assets/$flavor/launcher_icons/ic_launcher.png"'
-                  : '',
-            )
-            ..add('');
-        }
-        if (isMacOsEnabled) {
-          lines
-            ..add('      macos:')
-            ..add('        bundleId: "$org.$projectName$packageSuffix"')
-            ..add(
-              isGenerated
-                  ? '        icon: "flavor_assets/$flavor/launcher_icons/ic_launcher.png"'
-                  : '',
-            )
-            ..add('');
-        }
-
-        lines.add('');
-      }
-      final flavorLines = lines.join('\n');
-
-      var flavorContent = '';
-
-      if (isGenerated) {
-        flavorContent = pubspecFileContent.replaceAll(
-          flavorizrInjectKey,
-          flavorLines,
-        );
-      } else {
-        flavorContent = [pubspecFileContent, flavorLines].join('\n');
-      }
-      pubspecFile.writeAsStringSync(flavorContent);
-    } else {
-      flavorizrInjectKey.replaceAll(flavorizrInjectKey, '');
-      pubspecFile.writeAsStringSync(pubspecFileContent);
-    }
-
-    ///END:Flavorizer config injection
-  }
-
-  Future<void> _copyIcons(
-    String projectPath,
-    String flavor,
-    FlavorGeneratorParams params,
-  ) async {
-    await Directory('$projectPath/flavor_assets/$flavor/launcher_icons')
-        .create(recursive: true);
-    switch (flavor) {
-      case 'dev':
-      case 'prod':
-        await _iconCopy(
-          src: '$projectPath/assets/launcher_icons/ic_launcher_$flavor.png',
-          altSrc: '$projectPath/assets/launcher_icons/ic_launcher.png',
-          dst:
-              '$projectPath/flavor_assets/$flavor/launcher_icons/ic_launcher.png',
-        );
-
-        await _iconCopy(
-          src: '$projectPath/assets/android12splash_$flavor.png',
-          altSrc: '$projectPath/assets/android12splash.png',
-          dst: '$projectPath/flavor_assets/$flavor/android12splash.png',
-        );
-
-      default:
-        await _iconCopy(
-          src: '$projectPath/assets/launcher_icons/ic_launcher.png',
-          dst:
-              '$projectPath/flavor_assets/$flavor/launcher_icons/ic_launcher.png',
-          removeSrc: flavor == params.flavors.last,
-        );
-    }
-  }
-
-  Future<void> _iconCopy({
-    required String src,
-    required String dst,
-    String? altSrc,
-    bool removeSrc = true,
-  }) async {
-    var srcFile = File(src);
-
-    final srcFileExists = srcFile.existsSync();
-
-    ///Case when default flavor icons were removed
-    if (!srcFileExists) {
-      if (altSrc == null) {
-        return;
-      }
-
-      srcFile = File(altSrc);
-
-      ///Case when default icon was removed
-      if (!srcFile.existsSync()) {
-        return;
-      }
-    }
-
-    await srcFile.copy(dst);
-
-    if (srcFileExists && removeSrc) {
-      ///Remove default flavor icon
-      await srcFile.delete();
-    }
-  }
-
-  Future<void> _correctApple({
-    required String projectPath,
-    required String flavor,
-    required bool isGenerated,
-    required String platform,
-  }) async {
-    for (final run in ['Debug', 'Profile', 'Release']) {
-      final file = File('$projectPath/$platform/Flutter/$flavor$run.xcconfig');
-      final content = await file.readAsString();
-      final writer = file.openWrite()
-        ..write(
-          content.replaceAll(
-            'lib/main_$flavor.dart',
-            'lib/${isGenerated ? 'app/' : ''}flavors/main_$flavor.dart',
-          ),
-        );
-      await writer.flush();
-      await writer.close();
-    }
-  }
-
-  bool _isGenerated(String projectFolder) {
-    final pubspecFile = File('$projectFolder/pubspec.yaml');
-
-    return pubspecFile.existsSync() &&
-        pubspecFile.readAsStringSync().contains('#generated with mason');
   }
 
   Future<void> _generateConfigs({
@@ -566,71 +302,5 @@ $flavor:
 \tdart run flutter_native_splash:create
 ''');
     }
-  }
-
-  Future<void> _createFlavorBannerWidget({
-    required String projectPath,
-    required String projectName,
-    required bool isGenerated,
-    required ArchType? archType,
-  }) async {
-    final flavorBannerWidgetContent = '''
-import 'package:flutter/material.dart';
-import 'package:$projectName/flavors.dart';
-
-class FlavorBanner extends StatelessWidget {
-  const FlavorBanner({
-    required this.child,
-    super.key,
-  });
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    if (F.name == 'prod') return child;
-     return Banner(
-            message: F.name,
-            location: BannerLocation.topEnd,
-            child: child,
-        );
-  }
-}
-''';
-
-    String flavorBannerFilePath;
-
-    if (isGenerated) {
-      flavorBannerFilePath =
-          '$projectPath/lib/core/arch/widget/common/flavor_banner.dart';
-
-      if (archType == ArchType.simple) {
-        flavorBannerFilePath =
-            '$projectPath/lib/presentation/widget/common/flavor_banner.dart';
-      }
-
-      final appFile = File('$projectPath/lib/app/app.dart');
-
-      var appFileContent = await appFile.readAsString();
-
-      appFileContent = appFileContent
-          .replaceAll(
-            'child: widget ?? const SizedBox(),',
-            'child: FlavorBanner(child: widget ?? const SizedBox(),),',
-          )
-          .replaceAll(
-            'class App extends StatefulWidget {',
-            "import 'package:${flavorBannerFilePath.replaceAll('$projectPath/lib', projectName)}';\n\nclass App extends StatefulWidget {",
-          );
-
-      await appFile.writeAsString(appFileContent);
-    } else {
-      flavorBannerFilePath = '$projectPath/lib/flavor_banner.dart';
-    }
-
-    final flavorBannerFile = await File(flavorBannerFilePath).create(
-      recursive: true,
-    );
-    await flavorBannerFile.writeAsString(flavorBannerWidgetContent);
   }
 }
---
+++ lib/domain/service/file_generator_service/flavor_generator/mixins/flavor_generator_utils_mixin.dart
@@ -0,0 +1,245 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+import 'package:onix_flutter_bricks/domain/entity/arch_type/arch_type.dart';
+import 'package:onix_flutter_bricks/domain/service/base/base_generation_service.dart';
+import 'package:onix_flutter_bricks/domain/service/file_generator_service/flavor_generator/params/flavor_generator_params.dart';
+import 'package:onix_flutter_bricks/util/extension/swagger_extensions.dart';
+import 'package:onix_flutter_core/onix_flutter_core.dart';
+
+mixin FlavorGeneratorUtilsMixin
+    on BaseGenerationService<Result<int>, FlavorGeneratorParams> {
+  Future<String> getOrg(String name) async {
+    final appBuildGradleFile = File('$name/android/app/build.gradle');
+    var pbxProjFile = File('$name/ios/Runner.xcodeproj/project.pbxproj');
+
+    if (!pbxProjFile.existsSync()) {
+      pbxProjFile = File('$name/macos/Runner.xcodeproj/project.pbxproj');
+    }
+
+    if (appBuildGradleFile.existsSync()) {
+      final appBuildGradleContent = await appBuildGradleFile.readAsString();
+      final appBuildGradleContentLines = appBuildGradleContent.split('\n');
+
+      final applicationIdLine = appBuildGradleContentLines
+          .firstWhereOrNull((element) => element.contains('applicationId = '));
+
+      if (applicationIdLine != null) {
+        final lines = applicationIdLine
+            .split('applicationId = ')
+            .last
+            .replaceAll('"', '')
+            .split('.');
+
+        return lines.sublist(0, lines.length - 1).join('.');
+      }
+    }
+
+    if (pbxProjFile.existsSync()) {
+      final pbxProjContent = await pbxProjFile.readAsString();
+      final pbxProjContentLines = pbxProjContent.split('\n');
+
+      final applicationIdLines = pbxProjContentLines
+          .where((element) => element.contains('PRODUCT_BUNDLE_IDENTIFIER = '))
+          .map(
+            (e) => e
+                .replaceFirst('.RunnerTests', '')
+                .replaceLast(';', '')
+                .split('PRODUCT_BUNDLE_IDENTIFIER = ')
+                .last
+                .split('.'),
+          )
+          .toSet();
+
+      if (applicationIdLines.isEmpty) return '';
+
+      var orgSegments = applicationIdLines.first.toSet();
+
+      for (final org in applicationIdLines) {
+        orgSegments = orgSegments.intersection(org.toSet());
+      }
+
+      return orgSegments.toList().getRange(0, orgSegments.length - 1).join('.');
+    }
+
+    return '';
+  }
+
+  Future<void> copyIcons(
+    String projectPath,
+    String flavor,
+    FlavorGeneratorParams params,
+  ) async {
+    await Directory('$projectPath/flavor_assets/$flavor/launcher_icons')
+        .create(recursive: true);
+    switch (flavor) {
+      case 'dev':
+      case 'prod':
+        await _iconCopy(
+          src: '$projectPath/assets/launcher_icons/ic_launcher_$flavor.png',
+          altSrc: '$projectPath/assets/launcher_icons/ic_launcher.png',
+          dst:
+              '$projectPath/flavor_assets/$flavor/launcher_icons/ic_launcher.png',
+        );
+
+        await _iconCopy(
+          src: '$projectPath/assets/android12splash_$flavor.png',
+          altSrc: '$projectPath/assets/android12splash.png',
+          dst: '$projectPath/flavor_assets/$flavor/android12splash.png',
+        );
+
+      default:
+        await _iconCopy(
+          src: '$projectPath/assets/launcher_icons/ic_launcher.png',
+          dst:
+              '$projectPath/flavor_assets/$flavor/launcher_icons/ic_launcher.png',
+          removeSrc: flavor == params.flavors.last,
+        );
+    }
+  }
+
+  Future<void> _iconCopy({
+    required String src,
+    required String dst,
+    String? altSrc,
+    bool removeSrc = true,
+  }) async {
+    var srcFile = File(src);
+
+    final srcFileExists = srcFile.existsSync();
+
+    ///Case when default flavor icons were removed
+    if (!srcFileExists) {
+      if (altSrc == null) {
+        return;
+      }
+
+      srcFile = File(altSrc);
+
+      ///Case when default icon was removed
+      if (!srcFile.existsSync()) {
+        return;
+      }
+    }
+
+    await srcFile.copy(dst);
+
+    if (srcFileExists && removeSrc) {
+      ///Remove default flavor icon
+      await srcFile.delete();
+    }
+  }
+
+  Future<void> correctApple({
+    required String projectPath,
+    required String flavor,
+    required bool isGenerated,
+    required String platform,
+  }) async {
+    for (final run in ['Debug', 'Profile', 'Release']) {
+      final file = File('$projectPath/$platform/Flutter/$flavor$run.xcconfig');
+      final content = await file.readAsString();
+      final writer = file.openWrite()
+        ..write(
+          content.replaceAll(
+            'lib/main_$flavor.dart',
+            'lib/${isGenerated ? 'app/' : ''}flavors/main_$flavor.dart',
+          ),
+        );
+      await writer.flush();
+      await writer.close();
+    }
+  }
+
+  Future<void> createFlavorBannerWidget({
+    required String projectPath,
+    required String projectName,
+    required bool isGenerated,
+    required ArchType? archType,
+  }) async {
+    final flavorBannerWidgetContent = '''
+import 'package:flutter/material.dart';
+import 'package:$projectName/flavors.dart';
+
+class FlavorBanner extends StatelessWidget {
+  const FlavorBanner({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    if (F.name == 'prod') return child;
+     return Banner(
+            message: F.name,
+            location: BannerLocation.topEnd,
+            child: child,
+        );
+  }
+}
+''';
+
+    String flavorBannerFilePath;
+
+    if (isGenerated) {
+      flavorBannerFilePath =
+          '$projectPath/lib/core/arch/widget/common/flavor_banner.dart';
+
+      if (archType == ArchType.simple) {
+        flavorBannerFilePath =
+            '$projectPath/lib/presentation/widget/common/flavor_banner.dart';
+      }
+
+      final appFile = File('$projectPath/lib/app/app.dart');
+
+      var appFileContent = await appFile.readAsString();
+
+      appFileContent = appFileContent
+          .replaceAll(
+            'child: widget ?? const SizedBox(),',
+            'child: FlavorBanner(child: widget ?? const SizedBox(),),',
+          )
+          .replaceAll(
+            'class App extends StatefulWidget {',
+            "import 'package:${flavorBannerFilePath.replaceAll('$projectPath/lib', projectName)}';\n\nclass App extends StatefulWidget {",
+          );
+
+      await appFile.writeAsString(appFileContent);
+    } else {
+      flavorBannerFilePath = '$projectPath/lib/flavor_banner.dart';
+    }
+
+    final flavorBannerFile = await File(flavorBannerFilePath).create(
+      recursive: true,
+    );
+    await flavorBannerFile.writeAsString(flavorBannerWidgetContent);
+  }
+
+  Future<void> clean(
+      {required String projectPath, required bool isGenerated}) async {
+    await Process.run(
+      'rm',
+      ['-r', 'pages'],
+      workingDirectory: '$projectPath/lib',
+    );
+    await Process.run(
+      'rm',
+      ['main_dart.xml'],
+      workingDirectory: '$projectPath/.idea/runConfigurations',
+    );
+    if (isGenerated) {
+      await Process.run(
+        'rm',
+        ['ic_launcher.png'],
+        workingDirectory: '$projectPath/assets/launcher_icons',
+      );
+      await Process.run(
+        'rm',
+        ['app.dart'],
+        workingDirectory: '$projectPath/lib',
+      );
+    }
+  }
+}
