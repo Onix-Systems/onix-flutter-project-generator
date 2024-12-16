d97cb73bec14fe9bd1c2565cd9964635463aad8b
DemeoStep <demeo.alex@gmail.com>
2024-12-16 15:05:25 +0200

    hotfix: fix bricks

--- bricks/flutter_basic_base/hooks/post_gen.dart
+++ bricks/flutter_basic_base/hooks/post_gen.dart
@@ -14,6 +14,7 @@ const onix_flutter_core_models = 'onix_flutter_core_models: 0.0.3-beta.1';
 const onix_flutter_bloc = 'onix_flutter_bloc: 0.0.5-beta.1';
 const onix_flutter_provider = 'onix_flutter_provider: 0.0.3-beta.1';
 const onix_flutter_signals = 'onix_flutter_signals: 0.0.1';
+const onix_flutter_mvvm = 'onix_flutter_mvvm: 0.0.5';
 
 void run(HookContext context) async {
   name = context.vars['project_name'].toString().toSnakeCase;
@@ -179,7 +180,7 @@ Future<void> getDependencies(HookContext context) async {
     case 'signals':
       dependencies.addAll(['signals', onix_flutter_signals]);
     case 'viewModel':
-      dependencies.addAll(['onix_flutter_mvvm']);
+      dependencies.add(onix_flutter_mvvm);
     case 'base':
   }
 
