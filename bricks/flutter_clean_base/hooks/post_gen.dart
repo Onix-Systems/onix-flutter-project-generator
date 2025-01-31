34d068372f4bf14f68251fe5ac90423e89607c9d
DemeoStep <demeo.alex@gmail.com>
2025-01-31 14:16:35 +0200

    fix: lock freezed version to 2.5.7 and intl_utils to 2.8.8

--- bricks/flutter_clean_base/hooks/post_gen.dart
+++ bricks/flutter_clean_base/hooks/post_gen.dart
@@ -226,7 +226,7 @@ Future<void> getDependencies(HookContext context) async {
   if (context.vars['handLocalization']) {
     await Process.run('dart', ['pub', 'global', 'activate', 'flutter_gen']);
   } else {
-    dependencies.addAll(['intl', 'intl_utils']);
+    dependencies.addAll(['intl', 'intl_utils:2.8.8']);
   }
 
   if (context.vars['graphql']) {
