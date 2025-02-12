29ad22064d8fed8bc699dafc7953563eef011a56
DemeoStep <demeo.alex@gmail.com>
2025-02-12 10:51:44 +0200

    feat: fix brick

--- bricks/flutter_basic_base/hooks/pre_gen.dart
+++ bricks/flutter_basic_base/hooks/pre_gen.dart
@@ -84,6 +84,9 @@ Future<Map<String, dynamic>> _initCustomVars(HookContext context) async {
     'genWeb': context.vars['platforms'].contains('web'),
     'genWindows': context.vars['platforms'].contains('windows'),
     'genMacos': context.vars['platforms'].contains('macos'),
+    'genLinux': context.vars['platforms'].contains('linux'),
+    'genMobile': context.vars['platforms'].contains('ios') ||
+        context.vars['platforms'].contains('android'),
   };
 }
 
