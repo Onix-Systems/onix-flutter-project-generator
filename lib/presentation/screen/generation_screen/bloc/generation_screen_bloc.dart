75808d65ea3162092433fa4a43b899df2f375e41
DemeoStep <66169659+DemeoStep@users.noreply.github.com>
2025-09-30 12:03:16 +0300

    fix/flavorizr (#173)
    * fix: flavorizr run fix
    
    * feat: working with local bricks in debug mode
    
    * feat: upgrading packages for bricks
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode
    
    * feat: working with local bricks in debug mode

--- lib/presentation/screen/generation_screen/bloc/generation_screen_bloc.dart
+++ lib/presentation/screen/generation_screen/bloc/generation_screen_bloc.dart
@@ -23,7 +23,6 @@ import 'package:onix_flutter_bricks/domain/usecase/process/run_osascript_process
 import 'package:onix_flutter_bricks/domain/usecase/process/run_process_usecase.dart';
 import 'package:onix_flutter_bricks/domain/usecase/styles/generate_styles_usecase.dart';
 import 'package:onix_flutter_bricks/domain/usecase/swagger/create_swagger_components_usecase.dart';
-import 'package:onix_flutter_bricks/domain/usecase/swagger/get_swagger_components_usecase.dart';
 import 'package:onix_flutter_bricks/presentation/screen/generation_screen/bloc/generation_screen_bloc_imports.dart';
 import 'package:onix_flutter_bricks/util/commands.dart';
 import 'package:onix_flutter_bricks/util/enum/project_router.dart';
